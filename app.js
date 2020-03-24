037f208bd5ad208e00500f6c4f92a633b3f65a2f
Emil Martinov <martinov.emil@gmail.com>
2020-03-24 15:35:15 +0100

    Added 3 methods to Dinosaur constructor. Now the app randomly compares height, weight or diet with the user info

--- app.js
+++ app.js
@@ -82,24 +82,36 @@ const Dinos = [
   }
 ];
 
-function Human(species, weight, height, diet, image, name) {
+function Human(species, weight, diet, image, feet, inches, name) {
   this.species = species;
   this.weight = weight;
-  this.height = height;
-  // this.height = feet + inches/12
+  if (feet && inches) {
+    this.height = this.getHeightInInches(feet, inches);
+  }
   this.diet = diet;
   this.image = image;
+  this.feet = feet;
+  this.inches = inches;
   this.name = name;
 }
 
-Human.prototype.log = function() {
-  console.log(this);
+// Human.prototype.log = function() {
+//   console.log(this);
+// };
+
+Human.prototype.getHeightInInches = function(feet, inches) {
+  //coercing string values to numbers with (+)inches
+  return +feet * 12 + +inches;
 };
+
+// Human.prototype.compareHumanToDino = function(comparisonKey, Dino) {};
+// return function(comparisonKey, dinoSpecies) {
+//   console.log(self, typeof comparisonKey, dinoSpecies);
+//
 
 // it takes an array of strings, which are key values we want to exclude.
 Human.prototype.getRandomKeysWithExclusion = function(arrayOfKeysToExclude) {
   let keys = Object.keys(this); // get keys of object
-  console.log(keys);
 
   // For each exclusion `element` we filter the keys array and return the filtered array to be reduced for the next element. In the end we have an array filtered for the whole exlusion array elements.
   return arrayOfKeysToExclude.reduce(
@@ -108,52 +120,98 @@ Human.prototype.getRandomKeysWithExclusion = function(arrayOfKeysToExclude) {
   );
 };
 
-function Dinosaur(species, weight, height, diet, image, where, when, fact) {
-  Human.call(this, species, weight, height, diet, image);
+function Dinosaur(
+  species,
+  weight,
+  diet,
+  image,
+  where,
+  when,
+  fact,
+  height,
+  human
+) {
+  Human.call(this, species, weight, diet, image);
   this.where = where;
   this.when = when;
   this.fact = fact;
+  this.human = human; // reference to the human info object
+  this.height = height;
+  // this.randomFact = this.compareHumanToDino("diet", this.species);
 }
 
 Dinosaur.prototype = Object.create(Human.prototype);
+
+Dinosaur.prototype.compareHeight = function() {
+  return this.height > this.human.height
+    ? `${this.species} is taller ${this.height - this.human.height} than you`
+    : `You are taller than ${this.species}. ${this.species} is only ${this.height} inches tall`;
+};
+
+Dinosaur.prototype.compareWeight = function() {
+  return this.weight > this.human.weight
+    ? `${this.species} (${this.weight}) weighs ${this.weight -
+        this.human.weight} pounds heavier than you (${this.human.weight})`
+    : `You (${this.human.weight}) are heavier than ${this.species}. ${this.species} weighs only ${this.weight}`;
+};
+
+Dinosaur.prototype.compareDiet = function() {
+  return this.diet !== this.human.diet
+    ? `${this.species} is not a ${this.human.diet}. Instead they follow the ${this.diet} diet`
+    : `You follow the ${this.diet} diet as ${this.species}`;
+};
 
 const dinoCompare = document.getElementById("dino-compare");
 dinoCompare.addEventListener("submit", e => {
   e.preventDefault();
   document.querySelector(".form-container").style.display = "none";
-
+  const humanComparisonInfo = {
+    diet: e.target.diet.value.toLowerCase(),
+    weight: e.target.weight.value,
+    height: {
+      feet: e.target.feet.value,
+      inches: e.target.inches.value
+    }
+  };
   // coerce the feet value from string to number
-  const height = (+e.target.feet.value + e.target.inches.value / 18).toFixed(2);
+  // const height = (+e.target.feet.value + e.target.inches.value / 18).toFixed(2);
   const human = new Human(
     "Homo Sapiens",
-    e.target.weight.value,
-    height,
-    e.target.diet.value,
+    humanComparisonInfo.weight,
+    humanComparisonInfo.diet,
     "./images/human.png",
+    humanComparisonInfo.height.feet,
+    humanComparisonInfo.height.inches,
     e.target.name.value
   );
+
+  const Dinosaurs = Dinos.map(
+    dino =>
+      new Dinosaur(
+        dino.species,
+        dino.weight,
+        dino.diet,
+        dino.image,
+        dino.where,
+        dino.when,
+        dino.fact,
+        dino.height,
+        humanComparisonInfo
+      )
+  );
+
   dinoCompare.append(createComparisonDiv(Dinosaurs, human));
+  // get the computed height when human is initiliazed with Human constructor
+  humanComparisonInfo.heightInInches = human.height;
+  Object.freeze(humanComparisonInfo);
 });
 
-const Dinosaurs = Dinos.map(
-  dino =>
-    new Dinosaur(
-      dino.species,
-      dino.weight,
-      dino.height,
-      dino.diet,
-      dino.image,
-      dino.where,
-      dino.when,
-      dino.fact
-    )
-);
-
 const createComparisonDiv = (Dinosaurs, human) => {
-  const randomDinosaursSurroundingHuman = randomSelectWithOneStaticSelect(
-    Dinosaurs,
-    human
-  );
+  // Here we create a shuffled Dino array
+  // I could use const to get the same effect, but I want to
+  const randomDinosaursSurroundingHuman = shuffleArray(Dinosaurs);
+  // add human to the array
+  randomDinosaursSurroundingHuman.splice(4, 0, human);
 
   const getRandomFactofObjectWithExclusion = excludedArrayOfKeys => {
     const toCompareArray = human.getRandomKeysWithExclusion(
@@ -161,15 +219,19 @@ const createComparisonDiv = (Dinosaurs, human) => {
     );
     return toCompareArray[getRandomInt(toCompareArray.length)];
   };
+
   const comparisonDiv = document.createElement("section");
   comparisonDiv.id = "grid";
   comparisonDiv.innerHTML = randomDinosaursSurroundingHuman
     .map(dino => {
       if (dino.name !== undefined) {
+        // we only need to exclude if we do not want it to be compared explicitly
         const humanRandomKey = getRandomFactofObjectWithExclusion([
           "species",
           "image",
-          "name"
+          "name",
+          "inches", // instead we compare height in total inches
+          "feet"
         ]);
         console.log(human[humanRandomKey], typeof humanRandomKey);
         return `
@@ -182,7 +244,14 @@ const createComparisonDiv = (Dinosaurs, human) => {
       return `
       <div class="grid-item">
       <h2>${dino.species}</h2>
-      <p>${dino.fact}</p>
+      <p>${
+        dino.species === "Pigeon"
+          ? dino.fact
+          : // Here we select at random one of the compare methods and set its this to dino, otherwise since we use the array to call it, this points to the array.
+            [dino.compareDiet, dino.compareHeight, dino.compareWeight][
+              getRandomInt(3)
+            ].call(dino)
+      }</p>
       <img src="${dino.image}" alt="${dino.species}">
       </div>`;
     })
@@ -195,7 +264,7 @@ const insertAnElementAtIndex = (array, toInsert, index) => {
   array.splice(index, 0, toInsert);
 };
 
-const randomSelectWithOneStaticSelect = (array, staticSelect) => {
+const shuffleArray = array => {
   array = array.slice();
   const swapArrayElements = (array, swappedElementIndex, goToIndexPosition) => {
     const memoIndexPositionElement = array[goToIndexPosition];
@@ -209,6 +278,7 @@ const randomSelectWithOneStaticSelect = (array, staticSelect) => {
     j = getRandomInt(i);
     swapArrayElements(array, j, i);
   }
-  array.splice(4, 0, staticSelect);
+  // I thought making this function more generic means I can use it for any randomization
+  // array.splice(4, 0, staticSelect);
   return array;
 };
